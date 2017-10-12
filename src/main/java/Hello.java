eeff556192dc6703fb4fd42268dcd193d6662dd3
刘乃菲 <735180571@qq.com>
2017-10-12 11:03:13 +0800

    第二个版本

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -9,4 +9,12 @@ public class Hello {
     public String getPassword() {
         return password;
     }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
